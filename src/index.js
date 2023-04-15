7c3f54ebcf0aaa09927f07c569c4b49d05bfdc19
Shantanu <77541968+shantanu1998s@users.noreply.github.com>
2023-04-15 09:53:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 const fs = require("fs/promises");
 const fileName = "myfile.txt";
 const updateFile = async (fileName, fileContent) => {
-  // write code here
-  // dont change function name
+const path = require('path');
+
+function updateFile(fileName, contentToAdd) {
+  // read the content of the file
+  const filePath = path.join(__dirname, fileName);
+  let fileContent = fs.readFileSync(filePath, 'utf8');
+
+  // add the content to the existing content
+  fileContent += ', ' + contentToAdd;
+
+  // write the updated content back to the file
+  fs.writeFileSync(filePath, fileContent);
+
+  // return the updated content
+  return fileContent;
+}
+
+// example usage
+console.log(updateFile('myfile.txt', 'An online learning platform'));
+
   
 };
 
